fe657c1e281385ccecda5d7d58514fe57957f542
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-20 12:00:31 -0300

    Clase 2 react

--- Clase-2-React/src/Context/ProductContext.jsx
+++ Clase-2-React/src/Context/ProductContext.jsx
@@ -21,8 +21,29 @@ const ProductContextProvider = ({children}) => {
             id: 3
         },
     ])
+
+    function deleteProductById (product_id) {
+        const products_state_cloned = [...products]
+        for(let i = 0; i < products_state_cloned.length; i++){
+            const product = products[i]
+            if(product.id === product_id){
+                products_state_cloned.splice(i, 1)
+                break;
+            }
+        }
+        setProducts(products_state_cloned)
+    }
+
+    function deleteAllProducts (){
+      
+        setProducts([])
+    }
+
     const provider_values = {
-        products: products
+        products: products,
+        deleteAllProducts: deleteAllProducts,
+        deleteProductById: deleteProductById,
+        palabra_favorita: 'pepito'
     }
     return (
         <ProductContext.Provider value={provider_values}>
--- Clase-2-React/src/Screens/HomeScreen.jsx
+++ Clase-2-React/src/Screens/HomeScreen.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router'
+import { ProductContext } from '../Context/ProductContext'
 
 const HomeScreen = () => {
-  return (
-    <div>
-        <h1>Listado de productos</h1>
-        
+    //CONSUMIMOS EL CONTEXTO DE PRODUCTOS
+    const { products, palabra_favorita, deleteAllProducts, deleteProductById } = useContext(ProductContext)
 
-        <Link to={'/product/new'}>Ir a creador de productos</Link>
-        
-    </div>
-  )
+
+    const products_JSX = []
+    for (const product of products) {
+        products_JSX.push(
+            <div key={product.id}>
+                <h2>{product.title}</h2>
+                <div>Precio: ${product.price} ARS</div>
+                <button onClick={() => deleteProductById(product.id)}>Eliminar</button>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <h1>Listado de productos</h1>
+            {
+                products_JSX
+            }
+            <button onClick={deleteAllProducts}>Eliminar todos los productos</button>
+
+            <Link to={'/product/new'}>Ir a creador de productos</Link>
+
+        </div>
+    )
 }
 
 export default HomeScreen
+
+
+
+/*   
+              products.map(
+                    (product) => {
+                        return (
+                            <div>
+                                <h2>{product.title}</h2>
+                                <div>Precio: ${product.price} ARS</div>
+                                <button>Eliminar</button>
+                            </div>
+                        )
+                    }
+                ) */
