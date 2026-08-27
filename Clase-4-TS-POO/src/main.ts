37c8f5321d2259dfd51d2a455ec6e358e3791e80
matu.dev <mati.a.gimenez2002@gmail.com>
2026-08-27 10:36:10 -0300

    Clase 4

--- Clase-4-TS-POO/src/main.ts
+++ Clase-4-TS-POO/src/main.ts
@@ -10,14 +10,43 @@ class Persona {
     //Tipados de propiedad del objeto resultante persona
     nombre: string
     edad: number
+    dinero: number
+    sueldo: number
     constructor(nombre: string, edad: number){
         this.nombre = nombre
         this.edad = edad
+        this.dinero = 0
+        this.sueldo = 1200
+    }
+
+    presentarse (): void{
+        console.log(`Hola mi nombre es ${this.nombre} y tengo ${this.edad} años`)
+    }
+
+    cobrarSueldo(): void{
+        this.dinero = this.dinero + this.sueldo
+    }
+
+    cumplirAnio (): void{
+        this.edad = this.edad + 1
+    }
+
+    cambiarNombre(nuevo_nombre: string): void{
+        this.nombre = nuevo_nombre
+    }
+
+    aumentarSueldo (porcentaje_aumento: number): void{
+        this.sueldo = this.sueldo * ((porcentaje_aumento / 100) + 1)
     }
 }
 
 const persona_1 = new Persona('maria', 50)
-
+persona_1.aumentarSueldo(10)
+persona_1.cambiarNombre('mario')
+persona_1.cumplirAnio()
+persona_1.cobrarSueldo()
+persona_1.presentarse()
+console.log(persona_1)
 
 /* 
 En nuestro programa vamos a tener productos
@@ -29,3 +58,25 @@ Cada producto tendra:
 Declarar la clase Producto para poder tener productos en el programa
 Crear almenos 3 productos
 */
+
+
+
+class Producto{
+    titulo: string
+    precio: number
+    stock: number
+    id: number
+    constructor(titulo: string, precio: number, stock: number, id: number){
+        this.titulo = titulo
+        this.precio = precio
+        this.stock = stock
+        this.id = id
+    }
+
+
+}
+
+
+const cpu = new Producto("cpu", 50000, 30, 3)
+const gabinete = new Producto("gabinete", 5000, 100, 4)
+console.log(new Producto("ram", 200000000, 1, 5))
