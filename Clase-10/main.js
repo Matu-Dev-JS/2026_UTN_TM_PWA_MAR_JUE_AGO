e697e294079588969137b381156980b9b231d616
matu.dev <mati.a.gimenez2002@gmail.com>
2026-09-17 09:35:29 -0300

    Clase 10

---
+++ Clase-10/main.js
@@ -0,0 +1,4 @@
+
+console.log('hola mundo')
+console.log('Que tal?')
+console.log(1 + 2)
--- clase-7/tablas.sql
+++ clase-7/tablas.sql
@@ -27,6 +27,39 @@ CREATE TABLE miembros_espacio_trabajo(
     CONSTRAINT fk_id_espacio_de_trabajo_espacio_de_trabajo FOREIGN KEY(fk_id_espacio_de_trabajo) REFERENCES espacio_de_trabajo(id) ON DELETE CASCADE on UPDATE CASCADE
 );
 
+/* Canales */
+CREATE TABLE canales ( 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    nombre VARCHAR(30) NOT NULL, 
+    fk_id_espacio_trabajo INT NOT NULL, 
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    descripcion TEXT(3000),
+    CONSTRAINT fk_id_espacio_de_trabajo_canales FOREIGN KEY(fk_id_espacio_trabajo) REFERENCES espacio_de_trabajo(id) ON DELETE CASCADE on UPDATE CASCADE
+);
+
+
+/* Mensajes_Canales */
+CREATE TABLE mensajes_canales (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    contenido TEXT(10000) NOT NULL, 
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    fk_id_miembro INT NOT NULL,
+    fk_id_canal INT NOT NULL,
+    CONSTRAINT fk_id_miembro_mensajes_canales FOREIGN KEY(fk_id_miembro) REFERENCES miembros_espacio_trabajo(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_id_canal_mensajes_canales FOREIGN KEY(fk_id_canal) REFERENCES canales(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* Mensajes_Directos */
+CREATE TABLE mensajes_directos(
+	id int AUTO_INCREMENT not null PRIMARY KEY,
+	contenido TEXT(10000) NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	fk_id_receptor int not null,
+	fk_id_emisor int not null,
+	CONSTRAINT fk_id_receptor_mensajes_directos FOREIGN KEY (fk_id_receptor) REFERENCES miembros_espacio_trabajo(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_id_emisor_mensajes_directos FOREIGN KEY (fk_id_emisor) REFERENCES miembros_espacio_trabajo(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
 
 /*
 Crear las tablas:
