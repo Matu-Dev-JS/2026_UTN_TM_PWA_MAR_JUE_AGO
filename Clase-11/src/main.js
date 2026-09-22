fccc90e3562562ea2ae12e1fe50d7da4119b82ef
matu.dev <mati.a.gimenez2002@gmail.com>
2026-09-22 10:04:25 -0300

    Clase 11

--- Clase-11/src/main.js
+++ Clase-11/src/main.js
@@ -5,33 +5,67 @@ import connectMongoDB from "./config/mongo.config.js";
 import User from "./models/user.model.js";
 import Workspace from "./models/workspace.model.js";
 
-connectMongoDB()
 
 
 
 
-function createUser (){
-    User.create({nombre: 'maria', email:'maria@mail.com', password: 'pepe123'})
+function createUser() {
+    User.create({ nombre: 'maria', email: 'maria@mail.com', password: 'pepe123' })
 }
 
 //createUser()
 
 
-/* 
-Crear un schema de mongoose para el Workspace (espacio_de_trabajo)
-    - Seguir el mismo formato que en SQL (fecha_creacion, nombre, descripcion)
-    - Crear el schema y modelo dentro de /models/workspace.model.js
-Crear una funcion llamada createWorkspace(name, description) que inserte en la DB un espacio de trabajo
-*/
 
 function createWorkspace(name, description) {
     Workspace.create(
-        { 
-            nombre: name, 
-            descripcion: description 
+        {
+            nombre: name,
+            descripcion: description
         }
     )
 }
 
 
 /* createWorkspace('Contabilidad Argentina SA', 'Comunicaciones internas de la empresa') */
+
+
+
+/* 
+
+JS es un lenguaje programacion
+En JS existen 2 tipos de funciones: Sincronica / Asincronica
+Las diferenciamos porque en su declaracion llevan el async o no
+*/
+/* 
+En JS las promises (promesas) se inventaron para poder manejar procesos asincronicos
+Basicamente son un objeto con un estado interno
+Ese estado puede ser:
+    - pending: El proceso esta ocurriendo aun 
+    - resolved: El proceso finalizo exitosamente
+    - rejected: El proceso se interrumpio y finalizo
+*/
+
+
+async function getUsers() {
+    try {
+        console.log("Obteniendo usuarios")
+        //mediante fetch podemos emitir consultas HTTP
+        const result = await fetch(
+            'https://jsonplaceholder.typicode.com/users',
+            {
+                method: 'GET'
+            }
+        )
+        //Transformamos el contenido de la respuesta en JSON
+        const content = await result.json()
+        console.log("El contenido del fetch es:", content[0].name)
+    }
+    catch (error) {
+        console.log("Hubo un error:", error)
+    }
+}
+
+getUsers()
+console.log('Operacion importante')
+connectMongoDB()
